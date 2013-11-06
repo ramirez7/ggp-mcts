067863722477fb93a8dbe11441b2c7baba4e5edf
ramirez7 <armando.m.ramirez@gmail.com>
2013-11-06 15:27:08 -0500

    I used the relations environment to generate all legal moves. TODO: - comment more thoroughly - make function map to pass to MCTS

--- src/ggp_mcts/core.clj
+++ src/ggp_mcts/core.clj
@@ -1,4 +1,7 @@
-(ns ggp-mcts.core)
+(ns ggp-mcts.core
+  (:refer-clojure :exclude [==])
+  (:use [clojure.core.logic])
+  (:require [clojure.core.logic.fd :as fd]))
 
 ;;; This file will house the top-level functions needed for the user to
 ;;; create a Monte-Carlo Search Tree function for their game as described in
@@ -6,5 +9,84 @@
 ;;; - Game Description Language
 ;;; - core.logic relations
 ;;; - Clojure functions
+
+;;; Environment methods
+;;; An environment is a map of the following form:
+;;; {:relations {KEY-RELATION PAIRS}, :functions {KEY-FUNCTION PAIRS}}
+
+(defn get-binding [env path]
+  (partial (get-in env path) env))
+
+(defn get-relation [env name]
+  (get-binding env [:relations name]))
+
+(defn get-function [env name]
+  (get-binding env [:functions name]))
+
+;;; Relation->Function transformers
+
+;;; make-move [env move] (key :make-move)
+;;;   Updates the environment as the result of the move
+;;;   This results in a modified trueo relation
+;;;   The doeso relation is first modified to update state with nexto
+
+;;; TODO: COMMENT ALL THIS MORE AND EXPLAIN THOROUGHLY
+;;; moves is a map with keys as player names, and values of their moves
+;;; Right now, it generates code. If I change how moves are represented
+;;; it wouldn't be necessary. The does relation woudl have to take an arg
+;;; that is a vector of both r and a. Then it'd work.
+(defn update-does [env moves]
+  (let [next-does (list 'fn ['env 'r 'a] (concat '(conde) ;;DIRTY
+                                                 (map (fn [kv]
+                                                        [(list '== 'r (kv 0))
+                                                         (list '== 'a (kv 1))])
+                                                      moves)))]
+    (assoc-in env [:relations :does] (eval next-does))))
+
+(defn create-true [state]
+  (fn [env p]
+    (membero p state)))
+
+(defn update-true [env moves]
+  (let [next-state (distinct 
+                    (run* [q] 
+                      ((get-relation (update-does env moves) :next) q)))]
+    (assoc-in env [:relations :true] (create-true next-state))))
+
+;;; minikanren is great for permutations etc
+(defn comboo [pls mls c]
+  (conde
+    [(emptyo pls)
+     (emptyo mls)
+     (== c '())]
+    [(fresh [pa pd ma md choice res]
+       (conso pa pd pls)
+       (conso ma md mls)
+       (membero choice ma)
+       (conso [pa choice] res c)
+       (comboo pd md res))]))
+
+(defn assq->map [aq]
+  (reduce (fn [m v]
+            (assoc m (v 0) (v 1))) {} aq))
+
+(defn gen-children [env]
+  ;;players is a list of all players
+  ;;gen-legal is a function that generates all legal moves for player p
+  ;;legal-moves is a list of maps {PLAYER MOVE}
+  ;;legal-combos is all combinations of legal moves as an assq
+  ;;all-combos is that as maps (COMBINE WITH ABOVE?)
+  ;;Then it's as simple as updating the true in the env for each possible
+  ;; move combination and BOOM all children
+  (let [players (run* [q] ((get-relation env :role) q))
+        gen-legal (fn [p] 
+                    (run* [q]
+                      ((get-relation env :legal) p q)))
+        legal-moves (zipmap players (map gen-legal players))
+        legal-combos (run* [q]
+                       (comboo (keys legal-moves) (vals legal-moves) q))
+        all-combos (map assq->map legal-combos)]
+    (map (partial update-true env) all-combos)))
+
 
 
--- src/ggp_mcts/ttt_relations_map.clj
+++ src/ggp_mcts/ttt_relations_map.clj
@@ -22,33 +22,33 @@
 ;; Returns the relation bound to symbol name in environment env
 ;; Uses partial application to return a relation that doesn't need
 ;; an environment argument
-(defn get-binding [env name]
-  (partial (env (keyword name)) env))
+(defn- get-relation [env name]
+  (partial (get-in env [:relations (keyword name)]) env))
 
 ;; role(a) means that a is a role in the game.
 (defn roleo [env r]
   (conde
-    [(== r :x)]
-    [(== r :o)]))
+   [(== r :x)]
+   [(== r :o)]))
 
 ;; index isn't listed as a base relation
 ;; Maybe it should be part of baseo?
 (defn indexo [env i]
   (conde
-    [(== i 1)]
-    [(== i 2)]
-    [(== i 3)]))
+   [(== i 1)]
+   [(== i 2)]
+   [(== i 3)]))
 
 ;; input(r,a) means that a is an action for role r.
 (defn inputo [env r a]
   (conde
-    [(fresh [m n]
-       (== a [:mark m n]);;change to [:mark [m n]]??
-       ((get-binding env :roleo) r)
-       ((get-binding env :indexo) m)
-       ((get-binding env :indexo) n))]
-    [(== a :noop)
-     ((get-binding env :roleo) r)]))
+   [(fresh [m n]
+           (== a [:mark m n]);;change to [:mark [m n]]??
+           ((get-relation env :role) r)
+           ((get-relation env :index) m)
+           ((get-relation env :index) n))]
+   [(== a :noop)
+    ((get-relation env :role) r)]))
 
 ;; base(p) means that p is a base proposition in the game.
 ;; base propositions are represented as nested vectors
@@ -57,194 +57,198 @@
 ;; Maybe this relation isn't necessary given this representation
 (defn baseo [env p]
   (fresh [m n r]
-    (conde
-      [(== [:cell [m n :x]] p)
-       ((get-binding env :indexo) m)
-       ((get-binding env :indexo) n)]
-      [(== [:cell [m n :o]] p)
-       ((get-binding env :indexo) m)
-       ((get-binding env :indexo) n)]
-      [(== [:cell [m n :b]] p)
-       ((get-binding env :indexo) m)
-       ((get-binding env :indexo) n)]
-      [(== [:control [r]] p)
-       ((get-binding env :roleo) r)])))
+         (conde
+          [(== [:cell [m n :x]] p)
+           ((get-relation env :index) m)
+           ((get-relation env :index) n)]
+          [(== [:cell [m n :o]] p)
+           ((get-relation env :index) m)
+           ((get-relation env :index) n)]
+          [(== [:cell [m n :b]] p)
+           ((get-relation env :index) m)
+           ((get-relation env :index) n)]
+          [(== [:control [r]] p)
+           ((get-relation env :role) r)])))
 
 ;; init(p) means that the proposition p is true in the initial state.
 (defn inito [env p]
   (conde
-    [(== p [:cell [1 1 :b]])]
-    [(== p [:cell [1 2 :b]])]
-    [(== p [:cell [1 3 :b]])]
-    [(== p [:cell [2 1 :b]])]
-    [(== p [:cell [2 2 :b]])]
-    [(== p [:cell [2 3 :b]])]
-    [(== p [:cell [3 1 :b]])]
-    [(== p [:cell [3 2 :b]])]
-    [(== p [:cell [3 3 :b]])]
-    [(== p [:control [:x]])]))
+   [(== p [:cell [1 1 :b]])]
+   [(== p [:cell [1 2 :b]])]
+   [(== p [:cell [1 3 :b]])]
+   [(== p [:cell [2 1 :b]])]
+   [(== p [:cell [2 2 :b]])]
+   [(== p [:cell [2 3 :b]])]
+   [(== p [:cell [3 1 :b]])]
+   [(== p [:cell [3 2 :b]])]
+   [(== p [:cell [3 3 :b]])]
+   [(== p [:control [:x]])]))
 
 ;; true(p) means that the proposition p is true in the current state.
 ;; initial true relation is the init relation
 ;; This should be an input to the system to express state I think
 (defn trueo [env p]
-  ((get-binding env :inito) p))
+  ((get-relation env :init) p))
 
 ;; does(r,a) means that player r performs action a in the current state.
 ;; This should be an input to the system to express a move is made I think
+;; This should be empty or something?
 (defn doeso [env r a]
   (conde
-    [(== r :x)
-     (== a [:mark 1 1])]
-    [(== r :o)
-     (== a :noop)]))
+   [(== r :x)
+    (== a [:mark 1 1])]
+   [(== r :o)
+    (== a :noop)]))
 
 ;; next(p) means that the proposition p is true in the next state.
 (defn nexto [env p]
   (conde
-    [(fresh [m n r]
-       (== p [:cell [m n r]])
-       ((get-binding env :doeso) r [:mark m n])
-       ((get-binding env :trueo) [:cell [m n :b]]))]
-    [(fresh [m n w]
-       (== p [:cell [m n w]])
-       ((get-binding env :trueo) [:cell [m n w]])
-       (!= w :b))]
-    [(fresh [m n w j k]
-       (== p [:cell [m n :b]])
-       ((get-binding env :doeso) w [:mark j k])
-       ((get-binding env :trueo) [:cell [m n :b]])
-       (!= m j))]
-    [(fresh [m n w j k]
-       (== p [:cell [m n :b]])
-       ((get-binding env :doeso) w [:mark j k])
-       ((get-binding env :trueo) [:cell [m n :b]])
-       (!= n k))]
-    [(== p [:control [:x]])
-     ((get-binding env :trueo) [:control [:o]])]
-    [(== p [:control [:o]])
-     ((get-binding env :trueo) [:control [:x]])]))
+   [(fresh [m n r]
+           (== p [:cell [m n r]])
+           ((get-relation env :does) r [:mark m n])
+           ((get-relation env :true) [:cell [m n :b]]))]
+   [(fresh [m n w]
+           (== p [:cell [m n w]])
+           ((get-relation env :true) [:cell [m n w]])
+           (!= w :b))]
+   [(fresh [m n w j k]
+           (== p [:cell [m n :b]])
+           ((get-relation env :does) w [:mark j k])
+           ((get-relation env :true) [:cell [m n :b]])
+           (!= m j))]
+   [(fresh [m n w j k]
+           (== p [:cell [m n :b]])
+           ((get-relation env :does) w [:mark j k])
+           ((get-relation env :true) [:cell [m n :b]])
+           (!= n k))]
+   [(== p [:control [:x]])
+    ((get-relation env :true) [:control [:o]])]
+   [(== p [:control [:o]])
+    ((get-relation env :true) [:control [:x]])]))
 
 ;; legal(r,a) means it is legal for role r to play action a in the current state.
 ;; r \memberof {:x :o}
 ;; a is either [:mark M N] or :noop
 (defn legalo [env r a]
   (conde 
-    [(fresh [x y]
-       (== a [:mark x y])
-       ((get-binding env :trueo) [:cell [x y :b]])
-       ((get-binding env :trueo) [:control [r]]))]
-    [(== r :x)
-     (== a :noop)
-     ((get-binding env :trueo) [:control [:o]])]
-    [(== r :o)
-     (== a :noop)
-     ((get-binding env :trueo) [:control [:x]])]))
+   [(fresh [x y]
+           (== a [:mark x y])
+           ((get-relation env :true) [:cell [x y :b]])
+           ((get-relation env :true) [:control [r]]))]
+   [(== r :x)
+    (== a :noop)
+    ((get-relation env :true) [:control [:o]])]
+   [(== r :o)
+    (== a :noop)
+    ((get-relation env :true) [:control [:x]])]))
 
 ;; Supporting concepts and helpers
 (defn rowo [env m x]
   (fresh (_);;!! wtf
-    ((get-binding env :trueo) [:cell [m 1 x]])
-    ((get-binding env :trueo) [:cell [m 2 x]])
-    ((get-binding env :trueo) [:cell [m 3 x]])))
+         ((get-relation env :true) [:cell [m 1 x]])
+         ((get-relation env :true) [:cell [m 2 x]])
+         ((get-relation env :true) [:cell [m 3 x]])))
 
 (defn columno [env n x]
   (fresh (_);;!! wtf
-    ((get-binding env :trueo) [:cell [1 n x]])
-    ((get-binding env :trueo) [:cell [2 n x]])
-    ((get-binding env :trueo) [:cell [3 n x]])))
+         ((get-relation env :true) [:cell [1 n x]])
+         ((get-relation env :true) [:cell [2 n x]])
+         ((get-relation env :true) [:cell [3 n x]])))
 
 (defn diagonalo [env x]
   (conde
-    [((get-binding env :trueo) [:cell [1 1 x]])
-     ((get-binding env :trueo) [:cell [2 2 x]])
-     ((get-binding env :trueo) [:cell [3 3 x]])]
-    [((get-binding env :trueo) [:cell [1 3 x]])
-     ((get-binding env :trueo) [:cell [2 2 x]])
-     ((get-binding env :trueo) [:cell [3 1 x]])]))
+   [((get-relation env :true) [:cell [1 1 x]])
+    ((get-relation env :true) [:cell [2 2 x]])
+    ((get-relation env :true) [:cell [3 3 x]])]
+   [((get-relation env :true) [:cell [1 3 x]])
+    ((get-relation env :true) [:cell [2 2 x]])
+    ((get-relation env :true) [:cell [3 1 x]])]))
 
 (defn lineo [env x]
   (fresh [m]
-    (!= x :b)
-    (conde
-      [((get-binding env :rowo) m x)]
-      [((get-binding env :columno) m x)]
-      [((get-binding env :diagonalo) x)])))
+         (!= x :b)
+         (conde
+          [((get-relation env :row) m x)]
+          [((get-relation env :column) m x)]
+          [((get-relation env :diagonal) x)])))
 
 ;; goal(r,n) means that player the current state has utility n for player r.
 (defn goalo [env r n]
   (conde
-    [(== r :x)
-     (== n 100)
-     ((get-binding env :lineo) :x)
-     (nafc (get-binding env :lineo) :o)]
-    [(== r :x)
-     (== n 50)
-     (nafc (get-binding env :lineo) :x)
-     (nafc (get-binding env :lineo) :o)]
-    [(== r :x)
-     (== n 0)
-     (nafc (get-binding env :lineo) :x)
-     ((get-binding env :lineo) :o)]
-    [(== r :o)
-     (== n 100)
-     (nafc (get-binding env :lineo) :x)
-     ((get-binding env :lineo) :o)]
-    [(== r :o)
-     (== n 50)
-     (nafc (get-binding env :lineo) :x)
-     (nafc (get-binding env :lineo) :o)]
-    [(== r :o)
-     (== n 0)
-     ((get-binding env :lineo) :x)
-     (nafc (get-binding env :lineo) :o)]))
+   [(== r :x)
+    (== n 100)
+    ((get-relation env :line) :x)
+    (nafc (get-relation env :line) :o)]
+   [(== r :x)
+    (== n 50)
+    (nafc (get-relation env :line) :x)
+    (nafc (get-relation env :line) :o)]
+   [(== r :x)
+    (== n 0)
+    (nafc (get-relation env :line) :x)
+    ((get-relation env :line) :o)]
+   [(== r :o)
+    (== n 100)
+    (nafc (get-relation env :line) :x)
+    ((get-relation env :line) :o)]
+   [(== r :o)
+    (== n 50)
+    (nafc (get-relation env :line) :x)
+    (nafc (get-relation env :line) :o)]
+   [(== r :o)
+    (== n 0)
+    ((get-relation env :line) :x)
+    (nafc (get-relation env :line) :o)]))
 
 ;;terminal means that the current state is a terminal state.
 (defn openo [env]
   (fresh [m n]
-    ((get-binding env :trueo) [:cell [m n :b]])))
+         ((get-relation env :true) [:cell [m n :b]])))
 
 (defn terminalo [env]
   (fresh [w]
-    (conde
-      [((get-binding env :lineo) w)]
-      [(nafc openo)])));; nafc is experimental!!
+         (conde
+          [((get-relation env :line) w)]
+          [(nafc openo)])));; nafc is experimental!!
 
 ;; environment built from the above relations
 (def ttt-env
-  {:roleo roleo
-   :indexo indexo
-   :inputo inputo
-   :baseo baseo
-   :inito inito
-   :trueo trueo
-   :doeso doeso
-   :nexto nexto
-   :legalo legalo
-   :rowo rowo
-   :columno columno
-   :diagonalo diagonalo
-   :lineo lineo
-   :goalo goalo
-   :openo openo
-   :terminalo terminalo})
+  {:relations
+   {:role roleo
+    :index indexo
+    :input inputo
+    :base baseo
+    :init inito
+    :true trueo
+    :does doeso
+    :next nexto
+    :legal legalo
+    :row rowo
+    :column columno
+    :diagonal diagonalo
+    :line lineo
+    :goal goalo
+    :open openo
+    :terminal terminalo}})
 
 ;; Another doeso where :x marks (2,2) as opposed to (1,1)
 (defn alt-doeso [env r a]
   (conde
-    [(== r :x)
-     (== a [:mark 2 2])]
-    [(== r :o)
-     (== a :noop)]))
+   [(== r :x)
+    (== a [:mark 2 2])]
+   [(== r :o)
+    (== a :noop)]))
+
+
 
 ;; And the whole reason I did this:
 ;;
-;; ggp-mcts.core=> (set (run* [q] ((get-binding ttt-env :nexto) q)))
+;; ggp-mcts.core=> (set (run* [q] ((get-relation ttt-env :nexto) q)))
 ;; #{[:cell [3 3 :b]] [:cell [2 2 :b]] [:cell [3 2 :b]] [:cell [2 1 :b]]
 ;; [:cell [3 1 :b]] [:control [:o]] [:cell [1 1 :x]] [:cell [1 3 :b]] 
 ;; [:cell [2 3 :b]] [:cell [1 2 :b]]}
 ;;
-;; ggp-mcts.core=> (set (run* [q] ((get-binding (assoc ttt-env :doeso alt-doeso) 
+;; ggp-mcts.core=> (set (run* [q] ((get-relation (assoc ttt-env :doeso alt-doeso) 
 ;;                                              :nexto) q)))
 ;; #{[:cell [3 3 :b]] [:cell [1 1 :b]] [:cell [3 2 :b]] [:cell [2 1 :b]] 
 ;; [:cell [3 1 :b]] [:control [:o]] [:cell [2 2 :x]] [:cell [1 3 :b]] 
